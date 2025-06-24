ed5c47af4462bac64d759316028da9aad4899985
mahsanshoaib <bunnycotton101@gmail.com>
2025-06-24 11:33:50 +0500

    the first traning of python

---
+++ 01.py
@@ -0,0 +1,18 @@
+# Sys fundamentals
+import sys
+
+if len(sys.argv) !=3:
+    sys.exit()
+
+DNA = sys.argv[1]
+dna = sys.argv[2]
+
+print("DNA Sequence:", DNA)
+print("length of DNA is: ", len(DNA))
+print("dna sequence two is:", dna)
+print("Length of dna is:", len(dna))
+
+# import sys 
+# my_list = sys.argv[1]
+# print("my_list is:", my_list)  
+
